5015600c65986d5c16c8eb06b51bdd3ed8fc95a3
GunjiRujaswini <166891017+GunjiRujaswini@users.noreply.github.com>
2024-05-17 22:49:04 -0400

    Update 05-17-2024.R

--- 05-17-2024.R
+++ 05-17-2024.R
@@ -5,16 +5,16 @@ file_path <- "C:/Users/gruja/Downloads/Boston.csv"
 data <- read.csv(file_path)
 str(data)
 
-# Linear Regression: Let's assume we are using 'lstat' (percentage of lower status population) to predict 'medv' (median value of owner-occupied homes in $1000s)
-model_linear <- lm(medv ~ lstat, data=data)
+# Linear Regression: Let's assume we are using 'lstat' (percentage of lower status population) to predict 'rm' (median value of owner-occupied homes in $1000s)
+model_linear <- lm(rm ~ lstat, data=data)
 
 
 # Predictions
 data$pred_linear <- predict(model_linear)
 
 # Plotting
-plot(data$lstat, data$medv, main="Linear Regression: LSTAT vs MEDV",
-     xlab="LSTAT", ylab="MEDV", col="blue", pch=16)
+plot(data$lstat, data$rm, main="Linear Regression: LSTAT vs rm",
+     xlab="LSTAT", ylab="RM", col="blue", pch=16)
 lines(data$lstat, data$pred_linear, col="red", lwd=2)
 legend("topright", legend=c("Actual data", "Fitted line"), col=c("blue", "red"), pch=c(16, NA), lty=c(NA, 1))
 
@@ -34,20 +34,20 @@ legend("topright", legend=c("Actual data", "Fitted polynomial"), col=c("blue", "
 # Create a binary variable 'medv_high' (1 if medv > median, else 0)
 data$medv_high <- ifelse(data$medv > median(data$medv), 1, 0)
 
-# Logistic Regression: Using 'lstat' to predict 'medv_high'
-model_logistic <- glm(medv_high ~ lstat, data=data, family=binomial)
+# Create a sequence of ages for prediction
+age_seq <- seq(min(data$age), max(data$age), length.out = 100)
 
-# Predictions
-data$pred_logistic <- predict(model_logistic, type="response")
-data$pred_class <- ifelse(data$pred_logistic > 0.5, 1, 0)
+# Make predictions using the logistic regression model
+pred_prob <- predict(model_logistic, newdata = data.frame(age = age_seq), type = "response")
+
+# Convert predicted probabilities to predicted classes
+pred_class <- ifelse(pred_prob > 0.5, 1, 0)
 
 # Plotting
-plot(data$lstat, data$medv_high, main="Logistic Regression: LSTAT vs MEDV_HIGH",
-     xlab="LSTAT", ylab="MEDV_HIGH", col="blue", pch=16)
-points(data$lstat, data$pred_class, col="red", pch=4)
-legend("topright", legend=c("Actual data", "Predicted class"), col=c("blue", "red"), pch=c(16, 4))
+plot(data$age, data$medv_high, main = "Logistic Regression: AGE vs MEDV_HIGH",
+     xlab = "AGE", ylab = "MEDV_HIGH", col = "blue", pch = 16)
+lines(age_seq, pred_prob, col = "red", lwd = 2)
+legend("topright", legend = c("Actual data", "Predicted probability"), col = c("blue", "red"), pch = c(16, NA), lwd = c(NA, 2))
 
-# Check the structure of the data
-str(data)
-summary(data)
+
 
